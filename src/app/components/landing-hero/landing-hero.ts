6103ce5577df9aec751816534ba1e8c9dbb82d1d
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-03-29 19:34:49 +0800

    style: Improve the design of landing-hero and use new image

--- src/app/components/landing-hero/landing-hero.ts
+++ src/app/components/landing-hero/landing-hero.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollRevealDirective } from '../../helpers/scroll-reveal/scroll-reveal';
+
 
 @Component({
   selector: 'app-landing-hero',
   standalone: true,
-  imports: [CommonModule, ScrollRevealDirective],
+  imports: [CommonModule],
   templateUrl: './landing-hero.html',
   styleUrl: './landing-hero.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
