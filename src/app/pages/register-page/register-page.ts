9c90d73824741b6a69ad1eaa3f2ee6fe7e26a652
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-04-05 19:18:59 +0800

    feat; Added static registration page

--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,11 +1,20 @@
 ﻿import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
+  currentStep = 1;
+
+  nextStep() {
+    if (this.currentStep < 4) this.currentStep++;
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) this.currentStep--;
+  }
 
 }
