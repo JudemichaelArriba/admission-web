5a27e34f03fc4a5683448a79dbf1438095508ae4
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-04-07 07:08:33 +0800

    feat: Connect some buttons to routerLink

--- src/app/components/landing/landing-ending/landing-ending.ts
+++ src/app/components/landing/landing-ending/landing-ending.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollRevealDirective } from '../../../helpers/scroll-reveal/scroll-reveal';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-ending',
   standalone: true,
-  imports: [CommonModule, ScrollRevealDirective],
+  imports: [CommonModule, ScrollRevealDirective, RouterLink],
   templateUrl: './landing-ending.html',
   styleUrl: './landing-ending.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/components/landing/landing-hero/landing-hero.ts
+++ src/app/components/landing/landing-hero/landing-hero.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-hero',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-hero.html',
   styleUrl: './landing-hero.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
