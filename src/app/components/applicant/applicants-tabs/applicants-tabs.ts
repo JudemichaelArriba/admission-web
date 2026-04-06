1707f22a58a65356559dfe0b16b23e8496e105cc
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-04-07 06:50:43 +0800

    feat: Connected the logout fucntion to applicants tab

--- src/app/components/applicant/applicants-tabs/applicants-tabs.ts
+++ src/app/components/applicant/applicants-tabs/applicants-tabs.ts
@@ -1,6 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { DialogService } from '../../../services/dialog.service';
 
 @Component({
   selector: 'app-applicants-tabs',
@@ -12,11 +14,46 @@ export class ApplicantsTabsComponent {
   @Input() activeTab: string = 'status';
   @Output() tabChange = new EventEmitter<string>();
 
-
+  constructor(
+    private authService: AuthService,
+    private dialog: DialogService,
+    private router: Router
+  ) { }
 
   setTab(tab: string) {
     this.tabChange.emit(tab);
   }
 
+  logout(): void {
+    this.dialog.confirm(
+      'Sign Out',
+      'Are you sure you want to log out of your session?',
+      () => {
 
+        this.dialog.close();
+        this.performLogout();
+      },
+      () => {
+        this.dialog.close();
+      },
+      'Log Out',
+      'Cancel'
+    );
+  }
+
+  private performLogout(): void {
+
+
+    this.router.navigate(['/login']).then(() => {
+
+      this.authService.logout().subscribe({
+        next: () => {
+
+        },
+        error: () => {
+
+        }
+      });
+    });
+  }
 }
